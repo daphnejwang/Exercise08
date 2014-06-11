b431b5a522daf381611d783f875fec2b1c9d9896
Hackbright Student <info@hackbrightacademy.com>
2014-06-10 18:03:40 -0700

    finished program

--- markov.py
+++ markov.py
@@ -1,32 +1,48 @@
 #!/usr/bin/env python
 
 from sys import argv
+import random
 script, filename = argv
 
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
     input_text = corpus.read()
+    # print input_text
     words = input_text.split()
+    # print words
 
     chain_dict = {}
     for i in range(len(words) -2):
         tuple_words = (words[i], words[i+1])
-        chain_dict[tuple_words] = words[i+2]
+        if chain_dict.get(tuple_words, 0):
+            chain_dict[tuple_words].append(words[i+2])
+        else:
+            chain_dict[tuple_words] = [words[i+2]]
     # print chain_dict
     return chain_dict
 
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
-    original_text = "everyone cuddles"
-    
 
+    rand_tuple = random.choice(chains.keys())
+    # print "random tuple: %s" % str(rand_tuple)
+    paragraph = []
 
+    for indices in range(10):
+        found_value = chains.get(rand_tuple, 0)
+        if found_value == 0:
+            break
+        # print found_value
+        rand_word = random.choice(found_value)
+        rand_tuple = (rand_tuple[1],rand_word)
+        # print "random tuple: %s" % str(rand_tuple)
+        paragraph.append(rand_tuple[1])
 
-
-
-    return "Here's some random text."
+    text_string = ' '.join(paragraph)
+    # print text_string
+    return text_string
 
 def main():
     # args = sys.argv
@@ -40,7 +56,7 @@ def main():
 
     chain_dict = make_chains(input_text)
     random_text = make_text(chain_dict)
-    # print random_text
+    print random_text
 
 if __name__ == "__main__":
     main()
